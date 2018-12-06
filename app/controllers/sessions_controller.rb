d5710276e342b07ceaebef167c33f3fafe305750
github username <github email address>
2018-12-06 17:32:18 -0500

    add sessions controller action outline

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+
+  def new #creates a new session, login
+  end
+
+  def create #creates a new session, authenticates user
+  end
+
+  def destroy #logs out user by deleting session 
+  end
+
 end
