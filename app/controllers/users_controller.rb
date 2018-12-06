e04085a861aa1bdd0211c302115ad33b6687e68f
github username <github email address>
2018-12-06 17:28:39 -0500

    add user controller action method outline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController  #
 
-  def home
-  
+  def home  #displays option to log in
+
+  end
+
+  def show  #directs to users show page. Can view messages sent and responded to
+  end
+
+  def new  #directs to page to create new user
+    @user = User.new
+  end
+
+  def create  #creates a new user
+  end
+
+  def edit   #directs to page to edit user information
+  end
+
+  def update  #edit user info 
   end
 
   private
