4547ac30700fd37a024e5d22e2f028bd8e4b600b
github username <github email address>
2018-12-05 20:36:30 -0500

    nested resource revision

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "users#home"
-  resources :patients
-  resources :physicians do
-    resources :messages
+  resources :users do
+    resources :patients
+    resources :physicians
   end
-  resources :users
+
+    resources :patients do
+      resources :messages
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
