fd292741d90cf31e8316f7108602bf46fd12ae21
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-03-10 14:41:30 -0500

    removed model comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  scope :patients, -> { where(type: 'Patient') }  #scope method, same as defining a class method
+  scope :patients, -> { where(type: 'Patient') } 
   scope :physicians, -> { where(type: 'Physician') }
   has_many :physicians
   has_many :patients
