58c01fe4e54d3744997ff0eb8d595d0d25f8250e
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-10-06 16:03:57 -0400

    upgrade to rails 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,3 @@
 Rails.application.routes.draw do
-
-  root "application#welcome" 
-
-  get '/auth/facebook/callback' => 'sessions#create'
-
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-
-  get 'users/new_patient' => 'patients#new'
-  post 'users/new_patient' => 'users#create'
-
-  get 'users/new_physician' => 'physicians#new'
-  post 'users/new_physician' => 'users#create'
-
-  get  '/logout',   to: 'sessions#destroy', via: :delete
-
-  resources :users do  
-    resources :messages, only: [:new, :create, :edit, :update, :index, :destroy]
-    resources :responses, only: [:new, :create, :edit, :update, :destroy]
-    resources :patients, only: [:edit, :update]
-    resources :physicians, only: [:edit, :update]
-  end
-
-  #index, show, new, edit, create, update and destroy actions -resource
-  resources :categories do
-    resources :message_categories
-  end
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end #http://localhost:3000/rails/info/routes available routes, rails routes
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
