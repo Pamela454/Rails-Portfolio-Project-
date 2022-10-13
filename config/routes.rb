9d2b4191614da6638d9b23f1153482043fe0ec29
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-10-13 16:09:38 -0400

    upgrade ruby to 2.7.0 upgrade rails to 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "application#welcome" 
+
+  get '/auth/facebook/callback' => 'sessions#create'
+
+  get 'users/signin' => 'users#signin'
+  post 'sessions/create' => 'sessions#create'
+
+  get 'users/new_patient' => 'patients#new'
+  post 'users/new_patient' => 'users#create'
+
+  get 'users/new_physician' => 'physicians#new'
+  post 'users/new_physician' => 'users#create'
+
+  #get '/users/:user_id/responses/:id/edit' => 'users#edit_response'
+  #patch '/users/:user_id/messages/:id/edit' => 'messages#update'
+
+  get  '/logout',   to: 'sessions#destroy', via: :delete
+
+  resources :users do  
+    resources :messages, only: [:new, :create, :edit, :update, :index, :destroy]
+    resources :responses, only: [:new, :create, :edit, :update, :destroy]
+    resources :patients, only: [:edit, :update]
+  end
 end
