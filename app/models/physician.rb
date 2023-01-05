07a6a23140ddd76570074c7ccbb7b042f29012f6
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-05 08:54:44 -0500

    additional invalid inputs in create_physician_spec.rb

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -6,7 +6,7 @@ class Physician < User
   validates :name, :email, :npi, :specialty, :password_digest, presence: true
   validates :email, uniqueness: true
   validates :name, length: { minimum: 2 }
-  validates :npi, length: { minimum: 10 }
+  validates_length_of :npi, is: 10
 
 #validations specific to this model
 
--- spec/features/create_physician_spec.rb
+++ spec/features/create_physician_spec.rb
@@ -22,4 +22,70 @@ RSpec.describe 'Creating a physician user', type: :feature do
       click_on 'Create User'
       expect(page).to have_content("Name can't be blank")
     end
+
+    scenario 'invalid inputs' do
+      visit users_new_physician_path
+      fill_in 'Name', with: 'Dave'
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: 'HotSummer'
+      fill_in 'Npi', with: '1234567891'
+      fill_in 'Specialty', with: 'Family Medicine'
+      click_on 'Create User'
+      expect(page).to have_content("Email can't be blank")
+    end
+
+    scenario 'invalid inputs' do
+      visit users_new_physician_path
+      fill_in 'Name', with: 'Dave'
+      fill_in 'Email', with: 'dsmith@gmail.com'
+      fill_in 'Password', with: ''
+      fill_in 'Npi', with: '1234567891'
+      fill_in 'Specialty', with: 'Family Medicine'
+      click_on 'Create User'
+      expect(page).to have_content("Password can't be blank")
+    end
+
+    scenario 'invalid inputs' do
+      visit users_new_physician_path
+      fill_in 'Name', with: 'Dave'
+      fill_in 'Email', with: 'dsmith@gmail.com'
+      fill_in 'Password', with: 'HotSummer'
+      fill_in 'Npi', with: ''
+      fill_in 'Specialty', with: 'Family Medicine'
+      click_on 'Create User'
+      expect(page).to have_content("Npi can't be blank")
+    end
+
+    scenario 'invalid inputs' do
+      visit users_new_physician_path
+      fill_in 'Name', with: 'Dave'
+      fill_in 'Email', with: 'dsmith@gmail.com'
+      fill_in 'Password', with: 'HotSummer'
+      fill_in 'Npi', with: '123456789'
+      fill_in 'Specialty', with: 'Family Medicine'
+      click_on 'Create User'
+      expect(page).to have_content("Npi is the wrong length (should be 10 characters)")
+    end
+
+    scenario 'invalid inputs' do
+      visit users_new_physician_path
+      fill_in 'Name', with: 'Dave'
+      fill_in 'Email', with: 'dsmith@gmail.com'
+      fill_in 'Password', with: 'HotSummer'
+      fill_in 'Npi', with: '123456789134'
+      fill_in 'Specialty', with: 'Family Medicine'
+      click_on 'Create User'
+      expect(page).to have_content("Npi is the wrong length (should be 10 characters)")
+    end
+
+    scenario 'invalid inputs' do
+      visit users_new_physician_path
+      fill_in 'Name', with: 'Dave'
+      fill_in 'Email', with: 'dsmith@gmail.com'
+      fill_in 'Password', with: 'HotSummer'
+      fill_in 'Npi', with: '1234567891'
+      fill_in 'Specialty', with: ''
+      click_on 'Create User'
+      expect(page).to have_content("Specialty can't be blank")
+    end
   end
