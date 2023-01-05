b2f4ba33b536bd7d14767b2f7ffd2fb1174d60d2
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-05 09:57:18 -0500

    add invalid password input create_patient_spec.rb

--- spec/features/create_patient_spec.rb
+++ spec/features/create_patient_spec.rb
@@ -27,5 +27,14 @@ RSpec.describe 'Creating a patient user', type: :feature do
       click_on 'Create User'
       expect(page).to have_content("Email can't be blank")
     end
+
+    scenario 'invalid inputs' do
+      visit users_new_patient_path
+      fill_in 'Name', with: 'Linda'
+      fill_in 'Email', with: 'lsmith@gmail.com'
+      fill_in 'Password', with: ''
+      click_on 'Create User'
+      expect(page).to have_content("Password can't be blank")
+    end
   end
   
