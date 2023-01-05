abab80b3dc5180c751a819a7b24c139ff406431a
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-05 12:19:29 -0500

    add invalid input create_response_spec

--- spec/features/create_response_spec.rb
+++ spec/features/create_response_spec.rb
@@ -20,4 +20,18 @@ RSpec.describe 'Creating a Response', type: :feature do
     click_on 'Create Response'
     expect(page).to have_content('Looks like a rash')
   end
+
+  scenario 'invalid inputs' do
+    @physician = create(:physician)
+    @patient = create(:patient, id: 2)
+    @messages = create_list(:message, 11, patient_id: 2)
+    login_as(@patient)
+    page.set_rack_session(:user_id => @physician.id)
+    page.set_rack_session(:patient_id => @physician.id)
+    visit user_path(id: @physician.id)
+    first(:link, 'Respond to Question').click
+    fill_in 'Response', with: ''
+    click_on 'Create Response'
+    expect(page).to have_content("Response can't be blank")
+  end
 end
