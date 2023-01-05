4148159e06c7b0db7718de8d09a7c9646df8cbeb
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-05 10:43:14 -0500

    add invalid message title create_message_spec

--- spec/features/create_message_spec.rb
+++ spec/features/create_message_spec.rb
@@ -21,7 +21,21 @@ RSpec.describe 'Creating a Message', type: :feature do
     fill_in 'message_title', with: 'Sore Throat'
     fill_in 'message_question', with: 'I have a sore throat with white patches.'
     click_on 'Create Message'
-    visit user_path(id: @patient.id) 
-    expect(page).to have_content("Email: #{@patient.email}")
+    expect(page).to have_content("Sore Throat")
+  end
+
+  scenario 'invalid inputs' do
+    @patient = create(:patient)
+    @categories = create_list(:category, 11)
+    login_as(@patient)
+    page.set_rack_session(:user_id => @patient.id)
+    page.set_rack_session(:patient_id => @patient.id)
+    visit user_path(id: @patient.id)
+    click_on 'Ask A Question'
+    visit new_user_message_path(:user_id => @patient.id)
+    fill_in 'message_title', with: ''
+    fill_in 'message_question', with: 'I have a sore throat with white patches.'
+    click_on 'Create Message'
+    expect(page).to have_content("Title can't be blank")
   end
 end
