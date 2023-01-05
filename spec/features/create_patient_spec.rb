e50d75592951069ccf0e9f9ed58a58debc0d8add
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-05 09:53:25 -0500

    users create redirect correction create_patient_spec invalid input test

--- spec/features/create_patient_spec.rb
+++ spec/features/create_patient_spec.rb
@@ -9,5 +9,14 @@ RSpec.describe 'Creating a patient user', type: :feature do
       click_on 'Create User'
       expect(page).to have_content('lsmith@gmail.com')
     end
+
+    scenario 'invalid inputs' do
+      visit users_new_patient_path
+      fill_in 'Name', with: ''
+      fill_in 'Email', with: 'lsmith@gmail.com'
+      fill_in 'Password', with: 'SunnyDay'
+      click_on 'Create User'
+      expect(page).to have_content("Name can't be blank")
+    end
   end
   
