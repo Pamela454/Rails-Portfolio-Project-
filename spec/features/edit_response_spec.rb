67f29db7411d20bede7b8b2152fc39ce9da7084c
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-09 16:33:46 -0500

    add edit_response_spec file

---
+++ spec/features/edit_response_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Edit a Response', type: :feature do
+  scenario 'valid inputs' do
+    #login as physician user
+    #click on edit response link for a particular question
+    #enter response field
+    #click submit link
+    #check user show page for response data
+
+  end
+end
