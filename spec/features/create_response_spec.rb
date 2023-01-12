61a29fcf7e9d5b9de1d00c775edf5b837f9115b6
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-12 15:39:09 -0500

    create response as patient spec

--- spec/features/create_response_spec.rb
+++ spec/features/create_response_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Response', type: :feature do
   scenario 'valid inputs' do
-    #login as physician user
-    #click on response link for a particular question
-    #enter response field
-    #click submit link
-    #check user show page for response data
-
     @physician = create(:physician)
     @patient = create(:patient, id: 2)
     @messages = create_list(:message, 11, patient_id: 2)
@@ -33,5 +27,16 @@ RSpec.describe 'Creating a Response', type: :feature do
     fill_in 'Response', with: ''
     click_on 'Create Response'
     expect(page).to have_content("Response can't be blank")
+  end
+
+  scenario 'create response as a patient' do
+    patient = create(:patient)
+    physician = create(:physician, id: 2)
+    messages = create_list(:message, 11, patient_id: 1)
+    login_as(patient)
+    page.set_rack_session(:user_id => patient.id)
+    page.set_rack_session(:patient_id => patient.id)
+    visit user_path(id: patient.id)
+    expect(page).to have_no_content("Respond to Question")
   end
 end
