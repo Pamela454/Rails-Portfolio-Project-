6a39998ca64464784529366b03682c090fd07a2e
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-11 21:50:25 -0500

    patient_spec email formatter

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,6 +2,7 @@ class Patient < User
   has_secure_password
   belongs_to :user, optional: true
   has_many :messages, dependent: :destroy
+  validates :email, format: {with: /@/}  
   validates :name, :email, :password_digest, presence: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create 
   validates :name, length: { minimum: 2 }
