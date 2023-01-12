73426ae61a4911625f3fda741d3986c6647e3f65
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-11 22:00:26 -0500

    remove message controller pry

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,7 +25,6 @@ class MessagesController < ApplicationController
   end
 
   def create
-    binding.pry 
     @message = Message.new(message_params)
     if @message.save
       flash[:notice] = "Message successfully created"
