870c455b3391f1572f185d48056e97860ea75e6e
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-12 16:05:48 -0500

    edit response as patient spec

--- spec/features/edit_response_spec.rb
+++ spec/features/edit_response_spec.rb
@@ -36,4 +36,16 @@ RSpec.describe 'Edit a Response', type: :feature do
     click_on 'Update Response'
     expect(page).to have_content("Response can't be blank")
   end
+
+  scenario 'edit response as patient' do
+    physician = create(:physician)
+    patient = create(:patient, id: 2)
+    messages = create_list(:message, 11, patient_id: 2)
+    response = create(:response, message_id: 1, physician_id: 1)
+    login_as(patient)
+    page.set_rack_session(:user_id => patient.id)
+    page.set_rack_session(:physician_id => patient.id)
+    visit user_path(id: patient.id)
+    expect(page).to have_no_content("Edit Response")
+  end
 end
