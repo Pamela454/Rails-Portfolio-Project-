7acb44ae6bd489e02b0427d27d1019f89d031ca6
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-01-12 15:48:40 -0500

    delete message as physician spec

--- spec/features/delete_message_spec.rb
+++ spec/features/delete_message_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Message', type: :feature do
   scenario 'valid inputs' do
-
     @patient = create(:patient)
     @categories = create_list(:category, 11)
     @message = create(:message, patient_id: 1)
@@ -15,4 +14,17 @@ RSpec.describe 'Deleting a Message', type: :feature do
     expect(page).to have_no_content("Title: #{@message.title}")
     expect(page).to have_no_content("Question: #{@message.question}")
    end
+
+   scenario 'delete message as physician' do
+    physician = create(:physician)
+    patient = create(:patient)
+    categories = create_list(:category, 11)
+    message = create(:message, patient_id: 2)
+    login_as(physician)
+    page.set_rack_session(:user_id => physician.id)
+    page.set_rack_session(:patient_id => physician.id)
+    visit user_path(id: physician.id)
+    expect(page).to have_no_content("Delete A Question")
+   end
+
   end
