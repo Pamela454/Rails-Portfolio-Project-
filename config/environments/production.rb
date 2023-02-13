d9de10be9139f733ec06a2760eb6e86ada81328a
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-02-13 15:56:37 -0500

    gemfile pg added for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,14 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+end
+
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
