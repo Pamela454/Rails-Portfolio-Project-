685adccc37d19969e25a4900aaa16030ee32d701
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-02-15 16:38:03 -0500

    application.rb change

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ class StandaloneMigrations::MinimalRailtieConfig
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
+    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
