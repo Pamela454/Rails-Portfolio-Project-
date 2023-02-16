f24981210e479e4161688c85587e5342dc53e46b
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-02-16 15:59:52 -0500

    removed duplicate column migration

--- db/migrate/2018573925020_create_message_categories.rb
+++ db/migrate/2018573925020_create_message_categories.rb
@@ -3,7 +3,6 @@ class CreateMessageCategories < ActiveRecord::Migration[5.2]
     create_table :message_categories do |t|
       t.integer "message_id"
       t.integer "category_id"
-      t.integer "category_id"
       t.belongs_to :message, index: true
       t.belongs_to :category, index: true
       t.timestamps null: false
