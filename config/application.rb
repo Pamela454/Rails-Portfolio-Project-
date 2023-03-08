e4a11405b0821c23b26b1904ef4f490f4f8d58d2
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-03-08 15:15:57 -0500

    static assets line addition

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ class StandaloneMigrations::MinimalRailtieConfig
     config.load_defaults 5.0
     config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.serve_static_assets = true
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
